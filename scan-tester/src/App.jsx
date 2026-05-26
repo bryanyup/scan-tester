e63e9df4ba2fe8c3c913b7252c5eca36d7b8d27f
Bryan <bryan@gmail.com>
2026-05-26 13:14:10 +0700

    Final Scan Tester

--- scan-tester/src/App.jsx
+++ scan-tester/src/App.jsx
@@ -5,12 +5,16 @@ import { BarcodeDetector } from "barcode-detector/ponyfill";
 
 function App() {
 
-  const [scannedValue, setScannedValue] = useState('Nothing scanned yet')
-  const [currentScan, setCurrentScan] = useState('')
+  const [scannedValue, setScannedValue] = useState('')
+  const [scanCount, setScanCount] = useState(0)
+
 
   const videoRef = useRef(null)
   const canvasRef = useRef(null)
   const barcodeRef = useRef(null) //empty box labeled barcodeRef
+  const scanDialogRef = useRef(null)
+  const stopFrameRef = useRef(null)
+  const lastDialogRef = useRef(null)
 
   const [products, setProducts] = useState([
     { productID: "PR001", name: "Milk", quantity: 2 },
@@ -18,18 +22,61 @@ function App() {
     { productID: "PR003", name: "Bread", quantity: 6 }
   ])
 
-  const productBarcode = 'PR002'
-
-  const addQuantity = (barcode) => {
-    setProducts(prevProducts => //mandatory for reading the state
-      prevProducts.map(p =>
-        p.productID === barcode
-          ? { ...p, quantity: p.quantity + 1 }
-          : p
+  const addButton = () => {
+    if (scanCount === 0 || scannedValue === '') {
+      alert("Please scan your Products before adding")
+      return
+    }
+    else {
+      setProducts(prevProducts =>
+        prevProducts.map(p =>
+          p.productID === scannedValue ?
+            { ...p, quantity: p.quantity + scanCount } : p
+        )
       )
-    )
+      alert("Product Has been Added")
+      closeScanner()
+      setScannedValue('')
+      setScanCount(0)
+    }
   }
 
+  const productBarcode = "PR002"
+
+  const addQuantity = (barcode) => {
+    setProducts(prevProducts => { //mandatory for reading the state
+      const exist = prevProducts.some(p => p.productID === barcode)
+
+      if (exist) {
+        alert("Product has been added")
+        return prevProducts
+      }
+
+      else {
+        alert("Product does not exist")
+        return prevProducts
+      }
+    })
+  }
+
+  const openConfirmation = () => {
+    lastDialogRef.current?.showModal()
+  }
+
+  const closeConfirmation = () => {
+    lastDialogRef.current?.close()
+  }
+
+  const openScanner = () => {
+    scanDialogRef.current?.showModal()
+    scan()
+  }
+
+  const closeScanner = () => {
+    videoRef.current.srcObject?.getTracks().forEach(track => track.stop())
+    cancelAnimationFrame(stopFrameRef.current)
+    scanDialogRef.current?.close()
+  }
 
 
   useEffect(() => {
@@ -44,46 +91,69 @@ function App() {
       })
   }, [])
 
-    const scan = async () => {
+
+  const scan = async () => {
+
+    try {
       //Step 1. Setting Up the Camera
-      const camera = await navigator.mediaDevices.getUserMedia({ 
-        video: { facingMode: 'environment' } 
-      })
-      videoRef.current.srcObject = camera
+      const camera = await navigator.mediaDevices.getUserMedia({
+        video: { facingMode: 'environment' }
+      }) //if we want microphone we'll use audio
+      videoRef.current.srcObject = camera //basically its like 
+      // <video srv"camera"></video
       videoRef.current.play()
+
+      await new Promise(resolve => {
+        //promise means wait for something to finish then continue
+        //this is basically used for every event listener possible
+        videoRef.current.onloadedmetadata = () => resolve()
+        //runs when video is ready, onloadmetadata is when info loads
+      })
 
       //Step 2. Create the barcode Scanner
       const detector = new BarcodeDetector()
 
       //Step 3. Keep checking for barcodes
       const checkFrame = async () => {
-        const canvas = canvasRef.current
-        const ctx = canvas.getContext('2d')
 
-        //draw what the camera sees onto the canvas
-        ctx.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height)
+        // //try to detect a barcode
+        const barcode = await detector.detect(videoRef.current)
 
-        //try to detect a barcode
-        const barcode = await detector.detect(canvas)
-
-        //If we found the barcode we show it
-        if (barcode.length > 0) {
-          const scannedBarcode = barcode[0].rawValue
+        if (barcode.length > 0) { //if barcode is scanned
+          const [{ rawValue: scannedBarcode }] = barcode //raw value is the default 0bject given by the API library
           setScannedValue(scannedBarcode)
-          addQuantity(scannedBarcode)
-          alert("Product has Successfully been added")
 
-          videoRef.current.srcObject.getTracks().forEach(track => track.stop())
+          const product = products.find(p => p.productID === scannedBarcode)
+
+          if (!(product)) {
+            alert("Barcode is not valid")
+            alert('Added Product will be reset')
+            setScannedValue('')
+            setScanCount(0)
+          }
+
+          else {
+            alert("Product has been added")
+            setScanCount(prev => prev + 1)
+            setScannedValue(scannedBarcode)
+          }
+
+
+          // scannedProducts()
+          // addQuantity(scannedBarcode)
+          //  videoRef.current.srcObject.getTracks().forEach(track => track.stop())
         }
 
-        //keep checking every frame
-        requestAnimationFrame(checkFrame)
+        //used to constantly scan so you'll found the result
+        stopFrameRef.current = requestAnimationFrame(checkFrame)
+        //basically anything to constantly keep it running must use this
       }
-      checkFrame()
+      stopFrameRef.current = requestAnimationFrame(checkFrame)
+    } catch (error) {
+      console.error("Camera error:")
     }
 
-
-
+  }
 
   return (
     <>
@@ -91,26 +161,10 @@ function App() {
 
         <h1>Scanning in React</h1>
 
-        {/* Button For Scanning */}
-        <button
-            onClick={() => {
-              scan()
-            }}
-          >
-            Start Scanning Your Barcode
-          </button>
-        {/* Camera feed */}
-        <video
-          ref={videoRef}
-          style={{ width: 300 }}
-        ></video>
-
-        <canvas
-          ref={canvasRef}
-          width={300}
-          height={300}
-          style={{ display: 'none' }}
-        ></canvas>
+        <button onClick={() => {
+          openScanner()
+        }
+        }>Scanner Button</button>
 
         <h2>Scan Value: {scannedValue}</h2>
 
@@ -132,6 +186,41 @@ function App() {
           <svg ref={barcodeRef}></svg>
         </div>
       </div>
+
+      <dialog ref={scanDialogRef}>
+        <button onClick={() => {
+          closeScanner()
+        }}>X</button>
+        <div>Scan your barcode inside the dialog:</div>
+        {/* Camera feed */}
+
+        <video
+          ref={videoRef}
+          style={{ width: 300 }}
+        ></video>
+
+        <div className='div-scan'>
+          List of Scan:
+          <div>
+            <p>Product ID: {scannedValue}</p>
+            <p>Name: {products.find(p => p.productID === scannedValue)?.name
+              || "-"
+            }</p>
+            <p>Quantity: {scanCount}</p>
+          </div>
+          <button onClick={openConfirmation}>Add button</button>
+        </div>
+      </dialog>
+
+      <dialog ref={lastDialogRef}>
+        <p>Are you sure you want to finish adding this Product?</p>
+        <button onClick={() => {
+          addButton()
+          closeConfirmation()
+        }
+        }>Yes</button>
+        <button onClick={closeConfirmation}>No</button>
+      </dialog>
     </>
   )
 }
